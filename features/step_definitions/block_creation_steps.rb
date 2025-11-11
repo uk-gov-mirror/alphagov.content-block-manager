73da349e4476ee8836cdaf5571594d3fe3a5c54a
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-11-11 08:43:21 +0000

    Move block creation steps into their own step helper
    `content_block_manager_steps.rb` is already long and we now
    need to add a step helper for making a draft block (in the
    next commit).

---
+++ features/step_definitions/block_creation_steps.rb
@@ -0,0 +1,58 @@
+Given("a pension content block has been created") do
+  @content_blocks ||= []
+  @content_block = create(
+    :edition,
+    :pension,
+    details: { description: "Some text" },
+    creator: @user,
+    lead_organisation_id: @organisation.id,
+    title: "My pension",
+  )
+  Edition::HasAuditTrail.acting_as(@user) do
+    @content_block.publish!
+  end
+  @content_blocks.push(@content_block)
+end
+
+Given("a contact content block has been created") do
+  @content_blocks ||= []
+  @content_block = create(
+    :edition,
+    :contact,
+    details: { description: "Some text" },
+    creator: @user,
+    lead_organisation_id: @organisation.id,
+    title: "My contact",
+  )
+  Edition::HasAuditTrail.acting_as(@user) do
+    @content_block.publish!
+  end
+  @content_blocks.push(@content_block)
+end
+
+Given(/^([^"]*) content blocks of type ([^"]*) have been created with the fields:$/) do |count, block_type, table|
+  fields = table.rows_hash
+  organisation_name = fields.delete("organisation")
+  organisation = Organisation.all.find { |org| org.name == organisation_name }
+  title = fields.delete("title") || "title"
+  instructions_to_publishers = fields.delete("instructions_to_publishers")
+
+  (1..count.to_i).each do |_i|
+    document = create(:document, block_type.to_sym, sluggable_string: title.parameterize(separator: "_"))
+
+    editions = create_list(
+      :edition,
+      3,
+      block_type.to_sym,
+      document:,
+      lead_organisation_id: organisation.id,
+      details: fields,
+      creator: @user,
+      instructions_to_publishers:,
+      title:,
+    )
+
+    document.latest_edition = editions.last
+    document.save!
+  end
+end
--- features/step_definitions/content_block_manager_steps.rb
+++ features/step_definitions/content_block_manager_steps.rb
@@ -127,65 +127,6 @@ Then("I should be taken back to the document page") do
                                     ))
 end
 
-Given("a pension content block has been created") do
-  @content_blocks ||= []
-  @content_block = create(
-    :edition,
-    :pension,
-    details: { description: "Some text" },
-    creator: @user,
-    lead_organisation_id: @organisation.id,
-    title: "My pension",
-  )
-  Edition::HasAuditTrail.acting_as(@user) do
-    @content_block.publish!
-  end
-  @content_blocks.push(@content_block)
-end
-
-Given("a contact content block has been created") do
-  @content_blocks ||= []
-  @content_block = create(
-    :edition,
-    :contact,
-    details: { description: "Some text" },
-    creator: @user,
-    lead_organisation_id: @organisation.id,
-    title: "My contact",
-  )
-  Edition::HasAuditTrail.acting_as(@user) do
-    @content_block.publish!
-  end
-  @content_blocks.push(@content_block)
-end
-
-Given(/^([^"]*) content blocks of type ([^"]*) have been created with the fields:$/) do |count, block_type, table|
-  fields = table.rows_hash
-  organisation_name = fields.delete("organisation")
-  organisation = Organisation.all.find { |org| org.name == organisation_name }
-  title = fields.delete("title") || "title"
-  instructions_to_publishers = fields.delete("instructions_to_publishers")
-
-  (1..count.to_i).each do |_i|
-    document = create(:document, block_type.to_sym, sluggable_string: title.parameterize(separator: "_"))
-
-    editions = create_list(
-      :edition,
-      3,
-      block_type.to_sym,
-      document:,
-      lead_organisation_id: organisation.id,
-      details: fields,
-      creator: @user,
-      instructions_to_publishers:,
-      title:,
-    )
-
-    document.latest_edition = editions.last
-    document.save!
-  end
-end
-
 Then("I am taken back to Content Block Manager home page") do
   assert_equal current_path, root_path
 end
